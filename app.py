cd89c3fe5d85ab35565824409128e437bc54696f
Michael <mikejsmith1985@gmail.com>
2025-08-16 07:57:13 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("My Portfolio")
+st.header("About Me")
+st.write("First-time developer.")
+st.header("Projects")
+st.write("- Project 1: Description, GitHub link.")
+# Add more sections, images with st.image("path/to/image.png")
